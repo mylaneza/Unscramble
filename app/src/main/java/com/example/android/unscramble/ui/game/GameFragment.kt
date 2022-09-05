a15f5f19093b0120ed746de1fa8632dbf2a95c75
Ignacio Gonzalez <mylaneza@gmail.com>
2022-09-05 12:04:01 -0500

    Move Data To The ViewModel

--- app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
@@ -31,10 +31,6 @@ import com.example.android.unscramble.databinding.GameFragmentBinding
 class GameFragment : Fragment() {
 
     private val viewModel: GameViewModel by viewModels()
-    private var score = 0
-    private var currentWordCount = 0
-    private var currentScrambledWord = "test"
-
 
     // Binding object instance with access to the views in the game_fragment.xml layout
     private lateinit var binding: GameFragmentBinding
@@ -70,13 +66,13 @@ class GameFragment : Fragment() {
     * Displays the next scrambled word.
     */
     private fun onSubmitWord() {
-        currentScrambledWord = getNextScrambledWord()
+        /*currentScrambledWord = getNextScrambledWord()
         currentWordCount++
         score += SCORE_INCREASE
         binding.wordCount.text = getString(R.string.word_count, currentWordCount, MAX_NO_OF_WORDS)
         binding.score.text = getString(R.string.score, score)
         setErrorTextField(false)
-        updateNextWordOnScreen()
+        updateNextWordOnScreen()*/
     }
 
     /*
@@ -84,11 +80,11 @@ class GameFragment : Fragment() {
      * Increases the word count.
      */
     private fun onSkipWord() {
-        currentScrambledWord = getNextScrambledWord()
+       /* currentScrambledWord = getNextScrambledWord()
         currentWordCount++
         binding.wordCount.text = getString(R.string.word_count, currentWordCount, MAX_NO_OF_WORDS)
         setErrorTextField(false)
-        updateNextWordOnScreen()
+        updateNextWordOnScreen()*/
     }
 
     /*
@@ -133,6 +129,6 @@ class GameFragment : Fragment() {
      * Displays the next scrambled word on screen.
      */
     private fun updateNextWordOnScreen() {
-        binding.textViewUnscrambledWord.text = currentScrambledWord
+        binding.textViewUnscrambledWord.text = viewModel.currentScrambleWord
     }
 }
--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -3,4 +3,21 @@ package com.example.android.unscramble.ui.game
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
+
+    private var _score = 0
+
+    val score : Int
+        get() = _score
+
+    private var _currentWordCount = 0
+
+    val currentWordCount : Int
+        get() = _currentWordCount
+
+    private var _currentScrambledWord = "test"
+
+    val currentScrambleWord : String
+        get() = _currentScrambledWord
+
+
 }
